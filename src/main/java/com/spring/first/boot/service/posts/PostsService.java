6b1e05e73aba960bf33768b7732730044f61abaa
DoZeroJeong <dogug2865@gmail.com>
2020-03-16 10:05:34 +0900

    feat: posts creat and post test add

---
+++ src/main/java/com/spring/first/boot/service/posts/PostsService.java
@@ -0,0 +1,21 @@
+package com.spring.first.boot.service.posts;
+
+import com.spring.first.boot.domain.posts.PostsRepository;
+import com.spring.first.boot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+}
