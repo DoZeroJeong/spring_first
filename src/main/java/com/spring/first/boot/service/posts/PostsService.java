97bebc75561981639e6b1c59f4164b6abe826621
DoZeroJeong <dogug2865@gmail.com>
2020-03-17 14:09:56 +0900

    feat: PostList View add

--- src/main/java/com/spring/first/boot/service/posts/PostsService.java
+++ src/main/java/com/spring/first/boot/service/posts/PostsService.java
@@ -2,13 +2,16 @@ package com.spring.first.boot.service.posts;
 
 import com.spring.first.boot.domain.posts.Posts;
 import com.spring.first.boot.domain.posts.PostsRepository;
+import com.spring.first.boot.web.dto.PostsListResponseDto;
 import com.spring.first.boot.web.dto.PostsResponseDto;
 import com.spring.first.boot.web.dto.PostsSaveRequestDto;
 import com.spring.first.boot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import javax.transaction.Transactional;
+import org.springframework.transaction.annotation.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -33,6 +36,13 @@ public class PostsService {
     public PostsResponseDto findById (Long id) {
         Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 
 }
--- src/main/java/com/spring/first/boot/web/IndexController.java
+++ src/main/java/com/spring/first/boot/web/IndexController.java
@@ -1,15 +1,20 @@
 package com.spring.first.boot.web;
 
+import com.spring.first.boot.service.posts.PostsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
+    private final PostsService postsService;
+
     @GetMapping("/")
-    public String Index() {
+    public String index(Model model) {
+        model.addAttribute("posts", postsService.findAllDesc());
         return "index";
     }
 
